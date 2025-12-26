f4831fa6dd5d8b7c77c60bb70f6a3d0098fc0aad
jnawang <wangjunna2013@gmail.com>
2025-12-26 04:41:41 -0500

    update driver

--- workflows/03_01_prepare_data_for_driver_analysis.R
+++ workflows/03_01_prepare_data_for_driver_analysis.R
@@ -12,8 +12,8 @@ shelf(dplyr, lubridate, ggpubr, ggplot2, zoo, terra, corrplot)
 rm(list=ls())
 
 ####################Attention: change this directory based on your own directory of raw data
-# dir_rawdata <- '/Volumes/MaloneLab/Research/Stability_Project/Thermal_Acclimation'
-dir_rawdata <- '/Users/junnawang/YaleLab/data_server/'
+dir_rawdata <- '/Volumes/MaloneLab/Research/Stability_Project/Thermal_Acclimation'
+# dir_rawdata <- '/Users/junnawang/YaleLab/data_server/'
 ####################End Attention
 
 site_info <- read.csv(file.path('data', 'site_info.csv'))
